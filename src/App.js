2dbf964f2598a1d90af6946dfe4f3659f47b87dd
swgrouop <akai@ratocsystems.com>
2025-09-05 14:30:22 +0900

    ・ボタンによる表示切り替え

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,6 +18,45 @@ function App() {
         >
           Learn React V3
         </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [showLogo, setShowLogo] = useState(true);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={() => setShowLogo(!showLogo)}>
+          表示切り替え
+        </button>
+        {showLogo ? (
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React V3
+            </a>
+          </>
+        ) : (
+          <h1>Hello world!</h1>
+        )}
       </header>
     </div>
   );
